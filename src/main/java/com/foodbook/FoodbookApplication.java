266b43e453d3238bf510a45b9d882bdef6118db9
Rômulo Farias <romulodefarias@gmail.com>
2016-09-21 20:52:17 -0300

    Changed to the teacher's method of db connection + ignored applcation.properties

--- src/main/java/com/foodbook/FoodbookApplication.java
+++ src/main/java/com/foodbook/FoodbookApplication.java
@@ -3,6 +3,12 @@ package com.foodbook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import javax.sql.DataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 @EnableJpaRepositories(basePackages="com.foodbook.repository")
 @SpringBootApplication
@@ -10,5 +16,22 @@ public class FoodbookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodbookApplication.class, args);
+	}
+	
+	@Bean
+	@ConfigurationProperties(prefix="datasource.main")
+	public DataSource siteDataSourceBean(){
+		return DataSourceBuilder.create()
+								.build();
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean 
+		   siteEntityManagerFactoryBean(EntityManagerFactoryBuilder 
+				   						builder){
+		return builder.dataSource(siteDataSourceBean())
+					  .packages("com.foodbook.model")
+					  .persistenceUnit("foodbookPU")
+					  .build();
 	}
 }
