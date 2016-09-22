c25f3204de534f4f7d99924c23fa9548a6b1fc3c
Rômulo Farias <romulodefarias@gmail.com>
2016-09-21 21:00:17 -0300

    Minor fixes

--- src/main/java/com/foodbook/model/Address.java
+++ src/main/java/com/foodbook/model/Address.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
